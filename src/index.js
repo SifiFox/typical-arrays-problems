50e12d33e2c69863ea9c4ca9f107b92a3be5c1cf
SifiFox <dima.antonovich.1999@gmail.com>
2020-10-05 11:19:45 +0300

    feat: upd code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if(!array || array.length === 0) {return 0;}
+    return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if(!array || array.length === 0) {return 0;}
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if(!array || array.length === 0) {return 0;}
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += Number(array[i]);
+    }
+    return result / array.length;
 }
